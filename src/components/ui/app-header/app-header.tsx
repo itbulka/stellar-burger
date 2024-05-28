9b1541d059ce50127a7e733b7360406f00a58e49
Vladimir Povalskij <lokireads@gmail.com>
2024-05-28 15:58:57 +0300

    Сделал состояние линков на шапке

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -4,36 +4,66 @@ import {
   Logo,
   ProfileIcon
 } from '@zlden/react-developer-burger-ui-components';
+import clsx from 'clsx';
 import { FC } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import styles from './app-header.module.css';
 import { TAppHeaderUIProps } from './type';
-import clsx from 'clsx';
 
-export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
-  <header className={styles.header}>
-    <nav className={`${styles.menu} p-4`}>
-      <div className={styles.menu_part_left}>
-        <Link to='/' className={clsx(styles.link)}>
-          <BurgerIcon type={'disabled'} />
-          <p className='text text_type_main-default ml-2 mr-10'>Конструктор</p>
-        </Link>
-        <Link to='/feed' className={clsx(styles.link)}>
-          <ListIcon type={'primary'} />
-          <p className='text text_type_main-default ml-2'>Лента заказов</p>
-        </Link>
-      </div>
-      <div className={styles.logo}>
-        <Logo className='' />
-      </div>
-      <div className={styles.link_position_last}>
-        <Link to='/profile' className={clsx(styles.link)}>
-          <ProfileIcon type={'primary'} />
-          <p className='text text_type_main-default ml-2'>
-            {userName || 'Личный кабинет'}
-          </p>
-        </Link>
-      </div>
-    </nav>
-  </header>
-);
+export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => {
+  const location = useLocation();
+
+  return (
+    <header className={styles.header}>
+      <nav className={`${styles.menu} p-4`}>
+        <div className={styles.menu_part_left}>
+          <Link
+            to='/'
+            className={clsx(
+              styles.link,
+              location.pathname === '/' ? styles.link_active : ''
+            )}
+          >
+            <BurgerIcon
+              type={`${location.pathname === '/' ? 'primary' : 'secondary'}`}
+            />
+            <p className='text text_type_main-default ml-2 mr-10'>
+              Конструктор
+            </p>
+          </Link>
+          <Link
+            to='/feed'
+            className={clsx(
+              styles.link,
+              location.pathname === '/feed' ? styles.link_active : ''
+            )}
+          >
+            <ListIcon
+              type={`${location.pathname === '/feed' ? 'primary' : 'secondary'}`}
+            />
+            <p className='text text_type_main-default ml-2'>Лента заказов</p>
+          </Link>
+        </div>
+        <div className={styles.logo}>
+          <Logo className='' />
+        </div>
+        <div className={styles.link_position_last}>
+          <Link
+            to='/profile'
+            className={clsx(
+              styles.link,
+              location.pathname === '/profile' ? styles.link_active : ''
+            )}
+          >
+            <ProfileIcon
+              type={`${location.pathname === '/profile' ? 'primary' : 'secondary'}`}
+            />
+            <p className='text text_type_main-default ml-2'>
+              {userName || 'Личный кабинет'}
+            </p>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
