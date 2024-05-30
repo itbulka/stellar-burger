cb3fa8c57251fecb701243f2fb6b179a609426b9
Vladimir Povalskij <lokireads@gmail.com>
2024-05-30 12:22:58 +0300

    Исправил маршруты для модалок

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import '../../index.css';
 import styles from './app.module.css';
 
@@ -16,94 +16,102 @@ import {
 } from '../../pages';
 import { ProtectedRoute } from '../protected-route';
 
-const App = () => (
-  <>
-    <div className={styles.app}>
-      <AppHeader />
-      <Routes>
-        <Route path='/' element={<ConstructorPage />} />
-        <Route path='/feed' element={<Feed />} />
-        <Route
-          path='/login'
-          element={
-            <ProtectedRoute onlyUnAuth>
-              <Login />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path='/register'
-          element={
-            <ProtectedRoute onlyUnAuth>
-              <Register />
-            </ProtectedRoute>
-          }
-        />
-        <Route path='/forgot-password' element={<ForgotPassword />} />
-        <Route
-          path='/reset-password'
-          element={
-            <ProtectedRoute>
-              <ResetPassword />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path='/profile'
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path='/profile/orders'
-          element={
-            <ProtectedRoute>
-              <ProfileOrders />
-            </ProtectedRoute>
-          }
-        />
-        <Route path='*' element={<NotFound404 />} />
-      </Routes>
+const App = () => {
+  const location = useLocation();
 
-      <Routes>
-        <Route
-          path='/feed/:number'
-          element={
-            <Modal
-              title='Информация о заказе'
-              onClose={() => console.log('Click close')}
-            >
-              <OrderInfo />
-            </Modal>
-          }
-        />
-        <Route
-          path='/ingredients/:id'
-          element={
-            <Modal
-              title='Информация о заказе'
-              onClose={() => console.log('Click close')}
-            >
-              <IngredientDetails />
-            </Modal>
-          }
-        />
-        <Route
-          path='/profile/orders/:number'
-          element={
-            <Modal
-              title='Информация о заказе'
-              onClose={() => console.log('Click close')}
-            >
-              <OrderInfo />
-            </Modal>
-          }
-        />
-      </Routes>
-    </div>
-  </>
-);
+  const backgroundLocation = location.state?.background;
+
+  return (
+    <>
+      <div className={styles.app}>
+        <AppHeader />
+        <Routes location={backgroundLocation || location}>
+          <Route path='/' element={<ConstructorPage />} />
+          <Route path='/feed' element={<Feed />} />
+          <Route
+            path='/login'
+            element={
+              <ProtectedRoute onlyUnAuth>
+                <Login />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/register'
+            element={
+              <ProtectedRoute onlyUnAuth>
+                <Register />
+              </ProtectedRoute>
+            }
+          />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route
+            path='/reset-password'
+            element={
+              <ProtectedRoute>
+                <ResetPassword />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/profile'
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/profile/orders'
+            element={
+              <ProtectedRoute>
+                <ProfileOrders />
+              </ProtectedRoute>
+            }
+          />
+          <Route path='*' element={<NotFound404 />} />
+        </Routes>
+
+        {backgroundLocation && (
+          <Routes>
+            <Route
+              path='/feed/:number'
+              element={
+                <Modal
+                  title='Информация о заказе'
+                  onClose={() => console.log('Click close')}
+                >
+                  <OrderInfo />
+                </Modal>
+              }
+            />
+            <Route
+              path='/ingredients/:id'
+              element={
+                <Modal
+                  title='Информация о заказе'
+                  onClose={() => console.log('Click close')}
+                >
+                  <IngredientDetails />
+                </Modal>
+              }
+            />
+            <Route
+              path='/profile/orders/:number'
+              element={
+                <Modal
+                  title='Информация о заказе'
+                  onClose={() => console.log('Click close')}
+                >
+                  <OrderInfo />
+                </Modal>
+              }
+            />
+          </Routes>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default App;
